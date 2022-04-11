67c6776266a79fa34e1b8758f5c63adbb0499f97
PanghoChao <Sobrightlf@naver.com>
2022-04-12 02:26:17 +0900

    feat: lombok설치 및 yml 환경설정

--- src/main/java/com/shin/test/TestApplication.java
+++ src/main/java/com/shin/test/TestApplication.java
@@ -1,7 +1,10 @@
 package com.shin.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class TestApplication {
