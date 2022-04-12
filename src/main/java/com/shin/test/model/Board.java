2381f799591bc66d960fdce72bfc0645225de9bf
PanghoChao <Sobrightlf@naver.com>
2022-04-12 23:58:25 +0900

    feat: Reply(댓글) 테이블 작

--- src/main/java/com/shin/test/model/Board.java
+++ src/main/java/com/shin/test/model/Board.java
@@ -37,8 +37,6 @@ public class Board {
 	@JoinColumn(name = "userId")
 	private User user; // DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다.
 
-	@OneToMany(mappedBy="board", fetch= FetchType.EAGER) //mappedBy 연관관계의 주인이 아니다. DB에 칼럼을 만들지 마세요.
-	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
