e631bd1b308aad079126df40f3a5e6620060d68d
PanghoChao <Sobrightlf@naver.com>
2022-04-12 23:52:27 +0900

    feat: Board 테이블 작성(@Entity)

--- src/main/java/com/shin/test/model/Board.java
+++ src/main/java/com/shin/test/model/Board.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ import javax.persistence.OneToMany;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+@Entity
 public class Board {
 	
 	@Id //Primary key
